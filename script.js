a72305fa6ab3d9547a2ae58e4570878e18d1a6f3
Egael <egaelvalle@gmail.com>
2026-05-17 18:44:09 -0600

    upgrade

---
+++ script.js
@@ -0,0 +1,11 @@
+function enterSite() {
+  document.getElementById("ageGate").style.display = "none";
+}
+
+function exitSite() {
+    window.location.href = "https://es.wikipedia.org/wiki/Edad_m%C3%ADnima_legal_para_el_consumo_de_alcohol";
+}
+
+function toggleMenu() {
+  document.getElementById("mobileMenu").classList.toggle("active");
+}
